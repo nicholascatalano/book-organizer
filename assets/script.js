1a00a532553762de25db9961db38f3176f61bd88
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2023-12-06 13:54:24 -0500

    creates searchterm element

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,20 @@
 // DEPENDENCIES
 var searchFormEl = document.querySelector("#search-form");
-var searchResults = document.getElementById("#dynamic-results");
-
+var searchResults = document.getElementById("dynamic-results");
+var searchInput = document.getElementById("search-input");
+var searchTerm = searchInput.value;
 // DATA
 
 // FUNCTIONS
 function formSubmitHandler(event) {
   event.preventDefault();
+  searchTerm = searchInput.value;
   console.log("You clicked a button!");
+  console.log(searchTerm);
 }
 
 function getApi() {
-  var requestUrl = 'https://www.googleapis.com/books/v1/volumes?q=search+terms';
+  var requestUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + searchTerm;
   fetch(requestUrl)
     .then( function (response) {
       return response.json();
@@ -26,4 +29,5 @@ function getApi() {
 searchFormEl.addEventListener("submit", formSubmitHandler);
 
 // INITIALIZATION
-getApi();
+// getApi();
+
