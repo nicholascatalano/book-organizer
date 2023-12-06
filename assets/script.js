7129fe4a1365db0f477979fcbe4e2ba5fc8e252a
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2023-12-06 12:34:58 -0500

    adds google books api fetch

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 var searchFormEl = document.querySelector("#search-form");
+var searchResults = document.getElementById("#dynamic-results");
 
 // DATA
 
@@ -8,7 +9,21 @@ function formSubmitHandler(event) {
   event.preventDefault();
   console.log("You clicked a button!");
 }
+
+function getApi() {
+  var requestUrl = 'https://www.googleapis.com/books/v1/volumes?q=search+terms';
+  fetch(requestUrl)
+    .then( function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+
+}
+
 // USER INPUT
 searchFormEl.addEventListener("submit", formSubmitHandler);
 
 // INITIALIZATION
+getApi();
