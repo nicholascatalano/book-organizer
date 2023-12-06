cf00607692805c3180c1c0d46217be210d835e99
nickcatalano5 <nickcatalano24@gmail.com>
2023-12-06 12:04:58 -0500

    added form submit handler functionality

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,6 @@ var searchFormEl = document.querySelector("#search-form");
 // FUNCTIONS
 function formSubmitHandler(event) {
   event.preventDefault();
-
   console.log("You clicked a button!");
 }
 // USER INPUT
