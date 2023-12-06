b67854e2ccedc8bab3559b119621bad4911f3d02
nickcatalano5 <nickcatalano24@gmail.com>
2023-12-06 11:53:15 -0500

    fixed js link in html

--- scipt.js
+++ scipt.js
@@ -1,10 +1,13 @@
 // DEPENDENCIES
-var submitButton = document.querySelector("#submitButton");
+var searchFormEl = document.querySelector("#submit-form");
 
 // DATA
 
 // FUNCTIONS
-
+function formSubmitHandler(event) {
+    console.log("You clicked a button!");
+}
 // USER INPUT
+submitButton.addEventListener("click", formSubmitHandler)
 
 // INITIALIZATION
