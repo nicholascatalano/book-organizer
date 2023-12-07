c17404544eec9dfc06ffeb8528b8f847194109db
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2023-12-07 14:53:34 -0500

    saves books to local storage

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ var googleResults = document.getElementById("dynamic-google-results");
 var searchInput = document.getElementById("search-input");
 var savedBooksContainer = document.getElementById("saved-books-container");
 var savedBookList = document.getElementById("saved-book-list");
-
+var savedBooks = [];
 // DATA
 
 // FUNCTIONS
@@ -16,39 +16,39 @@ function formSubmitHandler(event) {
   getLocApi();
 }
 
-function getLocApi() {
-  // variable to hold LOC query url
-  var locQueryUrl = "https://www.loc.gov/books/?fo=json";
-  // splits and joins the search input using the correct format
-  searchTerm = searchInput.value.split(" ");
-  urlTerm = searchTerm.join("+");
+// function getLocApi() {
+//   // variable to hold LOC query url
+//   var locQueryUrl = "https://www.loc.gov/books/?fo=json";
+//   // splits and joins the search input using the correct format
+//   searchTerm = searchInput.value.split(" ");
+//   urlTerm = searchTerm.join("+");
 
-  // redefines the LOC query url to combine with the search query
-  locQueryUrl = locQueryUrl + "&q=" + urlTerm;
+//   // redefines the LOC query url to combine with the search query
+//   locQueryUrl = locQueryUrl + "&q=" + urlTerm;
 
-  // fetch API using the LOC query url
-  fetch(locQueryUrl)
-    .then(function (response) {
-      if (!response.ok) {
-        throw response.json();
-      }
+//   // fetch API using the LOC query url
+//   fetch(locQueryUrl)
+//     .then(function (response) {
+//       if (!response.ok) {
+//         throw response.json();
+//       }
 
-      return response.json();
-    })
-    // stores the data in locData
-    .then(function (locData) {
-      console.log(locData);
+//       return response.json();
+//     })
+//     // stores the data in locData
+//     .then(function (locData) {
+//       console.log(locData);
 
-      // if there is no data in the results section of the data array, return
-      if (!locData.results.length) {
-        console.log("No results found.");
-      } else searchResults.textContent = "";
-      // else print the results to the page using the printLocResults() function;
-      for (var i = 0; i < 8; i++) {
-        printLocResults(locData.results[i]);
-      }
-    });
-}
+//       // if there is no data in the results section of the data array, return
+//       if (!locData.results.length) {
+//         console.log("No results found.");
+//       } else searchResults.textContent = "";
+//       // else print the results to the page using the printLocResults() function;
+//       for (var i = 0; i < 8; i++) {
+//         printLocResults(locData.results[i]);
+//       }
+//     });
+// }
 
 function getGoogleApi() {
   searchTerm = searchInput.value.split(" ");
@@ -84,50 +84,95 @@ function printGoogleResults(googleData) {
   googlePageLink.textContent = googleTitle;
   googlePageLink.setAttribute("href", googleLink);
   
+  googleResults.innerHTML = "";
+
   googleInfoCard.append(googlePageLink);
   googleResults.append(googleInfoCard);
 
-}
+  var saveBookBtn = document.createElement("button");
+  googleResults.appendChild(saveBookBtn);
 
-function printLocResults(locData) {
-  console.log(locData);
-
-  // variable to hold card which will house LOC results info
-  var locInfoCard = document.createElement("div");
-  locInfoCard.classList.add("mb-2", "p-2", "text-dark", "bg-light", "card");
-
-  // variable to hold body of the LOC results info
-  var locInfoBody = document.createElement("div");
-  locInfoBody.classList.add("card-body");
-  locInfoCard.append(locInfoBody);
-
-  // variable to hold title of LOC result
-  var locTitleEl = document.createElement("h3");
-  locTitleEl.textContent = locData.title;
-
-  // variable to hold body content of loc results
-  var locBodyInfoEl = document.createElement("p");
-
-  // if statement to check if there is a value in the description of a result
-  if (locData.description) {
-    locBodyInfoEl.innerHTML =
-      "<strong>Description:</strong> " + locData.description[0] + "<br/>";
-  } else {
-    ("<strong>Description:</strong> No description for this result.");
+  saveBookBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    storeBooks();
+  }) 
+  function storeBooks() {
+    // check to see if "savedBooks" is in localStorage
+    if (localStorage.getItem("savedBooks")) {
+      savedBooks = JSON.parse(localStorage.getItem("savedBooks"))
+        } else {
+          savedBooks = [];
+        }
+    savedBooks.push({
+      googleTitle,
+      googleLink
+    });
+    localStorage.setItem("savedBooks", JSON.stringify(savedBooks));
   }
-
-  // button variable that redirects to LOC result
-  var locButtonEl = document.createElement("a");
-  locButtonEl.textContent = "Read More";
-  locButtonEl.setAttribute("href", locData.url);
-  locButtonEl.classList.add("btn", "btn-dark");
-
-  // appends title, body info (description), and read more button to body of card
-  locInfoBody.append(locTitleEl, locBodyInfoEl, locButtonEl);
-
-  // appends LOC info card to search results container
-  searchResults.append(locInfoCard);
+  renderBookList();
+  
 }
+
+// function printLocResults(locData) {
+//   console.log(locData);
+
+//   // variable to hold card which will house LOC results info
+//   var locInfoCard = document.createElement("div");
+//   locInfoCard.classList.add("mb-2", "p-2", "text-dark", "bg-light", "card");
+
+//   // variable to hold body of the LOC results info
+//   var locInfoBody = document.createElement("div");
+//   locInfoBody.classList.add("card-body");
+//   locInfoCard.append(locInfoBody);
+
+//   // variable to hold title of LOC result
+//   var locTitleEl = document.createElement("h3");
+//   locTitleEl.textContent = locData.title;
+
+//   // variable to hold body content of loc results
+//   var locBodyInfoEl = document.createElement("p");
+
+//   // if statement to check if there is a value in the description of a result
+//   if (locData.description) {
+//     locBodyInfoEl.innerHTML =
+//       "<strong>Description:</strong> " + locData.description[0] + "<br/>";
+//   } else {
+//     ("<strong>Description:</strong> No description for this result.");
+//   }
+
+//   // button variable that redirects to LOC result
+//   var locButtonEl = document.createElement("a");
+//   locButtonEl.textContent = "Read More";
+//   locButtonEl.setAttribute("href", locData.url);
+//   locButtonEl.classList.add("btn", "btn-dark");
+
+//   // appends title, body info (description), and read more button to body of card
+//   locInfoBody.append(locTitleEl, locBodyInfoEl, locButtonEl);
+
+//   // appends LOC info card to search results container
+//   searchResults.append(locInfoCard);
+// }
+
+function renderBookList() {
+  savedBookList.innerHTML = "";
+  for (var i = 0; i < savedBooksContainer.length; i++) {
+    var savedBook = savedBooks[i];
+    var li = document.createElement("li");
+    li.textContent = savedBook;
+    li.setAttribute("data-index", i);
+    var deleteBookBtn = document.createElement("button");
+    deleteBookBtn.textContent = "Clear Book From List";
+    li.appendChild(deleteBookBtn);
+    savedBookList.appendChild(li);
+  }
+}
+
+function init() {
+  renderBookList();
+}
+
+
+
 
 // USER INPUT
 searchFormEl.addEventListener("submit", formSubmitHandler);
